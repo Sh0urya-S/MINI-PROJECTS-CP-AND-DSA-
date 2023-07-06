7c867f56029dcaff8876cc59abab6f2d480b3035
Sh0urya-S <101881808+Sh0urya-S@users.noreply.github.com>
2023-07-06 15:30:09 +0530

    Add files via upload

---
+++ caesarcypher.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+void main()
+{	string encdec, msg, encdecmsg;
+	int shiftno;
+	cout << "Type encode to encrypt and decode to decrypt:";
+	cin >> encdec;
+	cout << "Type the message:";
+	cin >> msg;
+	cout << "Enter the number of shifts:";
+	cin >> shiftno;
+	if(encdec == "encode")
+		for (char& i:msg)
+			if (i == 'z')
+			{
+				i = 'a';
+				i = i + shiftno - 1;
+			}
+			else
+				i=i+shiftno;
+	else
+		for (char& i : msg)
+			if (i == 'a')
+			{
+				i = 'z';
+				i = i - shiftno + 1;
+			}
+			else
+				i=i-shiftno;
+	cout << "The " << encdec << "d message is " << msg;
+}
